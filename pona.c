c2a46a05272839cbcaed03804ce7e90690164471
ParimalaDhandapani <parimala7273@gmail.com>
2016-10-20 11:17:41 +0530

    Create pona.c

---
+++ pona.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+printf("Enter the number");
+scanf("%i",&a);
+if(a>0)
+printf(" it positive");
+else if(a<0)
+printf("it negative");
+else 
+printf("the number is zero");
+return 0;
+}
+
+
